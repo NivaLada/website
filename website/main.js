c88895355490d3a965a714704a8d4e4f8144217e
Lucas Murtfeld <lucasmurtfeld7@gmail.com>
2021-12-09 23:41:41 +0100

    final first stage

--- website/main.js
+++ website/main.js
@@ -1,8 +1,12 @@
 const img1 = document.getElementById("img1")
 const img2 = document.getElementById("img2")
+const img3 = document.getElementById("img3")
+const img4 = document.getElementById("img4")
 
 window.addEventListener("scroll", function(){
     let offset = window.pageYOffset;
-    img1.style.backgroundPositionY = offset * -0.3 + "px";
+    img1.style.backgroundPositionY = offset * -0.2 + "px";
     img2.style.backgroundPositionY = offset * -0.08 + "px";
+    img3.style.backgroundPositionY = offset * -0.036 + "px";
+    img4.style.backgroundPositionY = offset * -0.028 + "px";
 })
